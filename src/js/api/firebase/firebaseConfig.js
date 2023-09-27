6757a9ac5bdfe4fbec9aedb59d319042ef301b8a
Dmytro <moiseenkodmitriy@i.ua>
2023-09-27 17:12:53 +0300

    dep

--- src/js/api/firebase/firebaseConfig.js
+++ src/js/api/firebase/firebaseConfig.js
@@ -3,7 +3,17 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { FIREBASE_CONFIG } from '../../constants/envConsts';
 
-export const firebaseConfig = JSON.parse(FIREBASE_CONFIG);
+export const firebaseConfig = {
+  apiKey: FIREBASE_CONFIG,
+  authDomain: 'filmoteka-a6ea9.firebaseapp.com',
+  databaseURL:
+    'https://filmoteka-a6ea9-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'filmoteka-a6ea9',
+  storageBucket: 'filmoteka-a6ea9.appspot.com',
+  messagingSenderId: '824201120756',
+  appId: '1:824201120756:web:f8f623736e98ae62edc7a1',
+  measurementId: 'G-THM4WVYQNH',
+};
 
 export const db = getFirestore(initializeApp(firebaseConfig));
 export const auth = getAuth();
