f25b959f0eef066ceb0da25e369c584fee627437
Dmytro <moiseenkodmitriy@i.ua>
2023-09-27 19:20:05 +0300

    deploy

--- src/js/constants/envConsts.js
+++ src/js/constants/envConsts.js
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const FIREBASE_CONFIG = process.env.FIREBASE_CONFIG;
+export const FIREBASE_KEY = process.env.FIREBASE_KEY;
 export const TMDB_KEY = process.env.TMDB_KEY;
